27f66d8fb6548e8a093774d61e4b0d7a33304702
Kee <shikee.maloney@codeimmersives.com>
2023-03-06 08:39:13 -0500

    all

--- rick-n-morty/src/App.js
+++ rick-n-morty/src/App.js
@@ -5,9 +5,35 @@ import Cards from "./Cards";
 
 
 function App() {
+  const [cardList, setCardList] = React.useState({})
+
+  async function getAPI() {
+    await fetch(`https://rickandmortyapi.com/api/character/1`)
+      .then(response => response.json())
+      .then(data => setCardList(data));
+  }
+
+  React.useEffect(() => {
+    getAPI();
+    console.log(cardList)
+  }, []);
+
+ 
+
   return (
-    <div>
-      <Cards />
+    <div className="container-sm">
+      <Cards
+      text = {cardList}
+      />
+      <Cards
+      text = {cardList}
+      />
+      <Cards
+      text = {cardList}
+      />
+      <Cards
+      text = {cardList}
+      />
     </div>
   );
 }
--- rick-n-morty/src/Cards.js
+++ rick-n-morty/src/Cards.js
@@ -1,12 +1,15 @@
 import React from "react"
 
-function Cards() {
+function Cards(props) {
     return (
-        <div className="card" width='18rem'>
-            <img src="..." className="card-img-top" alt="..."/>
+        <div className="card" width='50 px' height='50px'>
+            <img src={props.text.image} className="card-img-top" alt="..." height='50 px' width='50 px'/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">{props.text.name}</p>
+                    <p className="card-text">{props.text.status}</p>
+                    <p className="card-text">{props.text.species}</p>
+                    <p className="card-text">{props.text.gender}</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
         </div>
